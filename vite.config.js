b94d96a15379750252bd01043d5816b25c4d0935
oscar <oro106@gmail.com>
2025-10-31 08:45:49 +0100

    Finished project. Now app is a PWA. Everything Ok.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,56 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'android-chrome-192x192.png', 
+        'android-chrome-512x512.png', 
+        'apple-touch-icon.png',
+        'favicon-16x16.png',
+        'favicon-32x32.png',
+        'favicon.ico'
+      ],
+      manifest: {
+        name: 'MiniReddits',
+        short_name: 'MiniReddits',
+        description: 'Browse and search Reddit posts easily.',
+        theme_color: '#FFA500',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png'
+          },
+          {
+            src: 'favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png'
+          },
+          {
+            src: 'favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   test: {
     environment: 'jsdom',
     globals: true,
